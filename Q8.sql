29ccbb20a289a7cf4965499bd1a23b819d702f23
Mingyang-Li <mingyangli118@gmail.com>
2021-05-14 12:20:33 +1200

    Half done q8

--- Q8.sql
+++ Q8.sql
@@ -4,15 +4,33 @@
 - UnitPrice - SalesOrderDetail
 - LineTotal - SalesOrderDetail
 - Name - Product
-- Description - ProductDescription
+- Description - 
+    Product (ProductModelID) => 
+        ProductModel (ProductModelProductDescriptionID) => 
+            ProductModelProductDescription (ProductDescriptionID) => 
+                ProductDescription.Description
 */
 
 SELECT 
     SalesOrderDetailID,
     OrderQty,
     UnitPrice,
+	UnitPriceDiscount,
     LineTotal,
-    Product.Name,
+    Product.Name as ProductName,
     ProductDescription.Description
-FROM ...
+FROM SalesLT.SalesOrderDetail
 
+JOIN SalesLT.Product
+ON SalesLT.SalesOrderDetail.ProductID = SalesLT.Product.ProductID 
+
+JOIN SalesLT.ProductModel 
+ON Product.ProductModelID = ProductModel.ProductModelID
+
+JOIN SalesLT.ProductModelProductDescription
+ON ProductModelProductDescription.ProductModelID = Product.ProductModelID
+
+JOIN SalesLT.ProductDescription
+ON ProductModelProductDescription.ProductDescriptionID = ProductDescription.ProductDescriptionID
+
+WHERE UnitPriceDiscount = 0.00 -- without discounts
