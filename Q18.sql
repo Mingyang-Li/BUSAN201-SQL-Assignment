28959e1dfa39ca0c873c5e5e87ff14355bf1042e
Mingyang-Li <mingyangli118@gmail.com>
2021-05-15 23:23:57 +1200

    Done q18

--- Q18.sql
+++ Q18.sql
@@ -1,5 +1,64 @@
 /*
-How many units of products categorised as components have been ordered? Only include units that have been or will be shipped to the Canadian province of Quebec.
+How many units of products categorised as components have been ordered? 
+Only include units that have been or will be shipped to the Canadian province of Quebec.
 
-Products belonging to sub-categories (a.k.a. child categories) of a parent category are considered products of that parent category. E.g. products which belong to sub-categories of "Bikes" (such as "Road Bikes" and "Mountain Bikes") are also considered "bike" products.
+Products belonging to sub-categories (a.k.a. child categories) of a parent category are considered products of that parent category. 
+E.g. products which belong to sub-categories of "Bikes" (such as "Road Bikes" and "Mountain Bikes") are also considered "bike" products.
+
+SalesOrderHeader {
+    FK: [SalesOrderID],
+    Data: [ShipToAddressID]
+}
+
+Address
+
+SalesOrderDetail {
+    PK: SalesOrderID,
+    FK: [ProductID]
+}
+
+Product {
+    PK: ProductID,
+    FK: [ProductCategoryID]
+}
+
+ProductCategory {
+    PK: ProductCategoryID,
+    Data: [Name]
+}
 */
+
+SELECT 
+	SUM(SalesLT.SalesOrderDetail.OrderQty) AS 'Total Units Ship to Quebec'
+	/*
+    SalesLT.SalesOrderHeader.SalesOrderID,
+    SalesLT.SalesOrderHeader.ShipToAddressID,
+	SalesLT.Address.StateProvince,
+	SalesLT.Address.CountryRegion,
+    SalesLT.SalesOrderDetail.ProductID,
+	SalesLT.SalesOrderDetail.OrderQty,
+    SalesLT.Product.ProductID,
+    SalesLT.ProductCategory.ProductCategoryID,
+    SalesLT.ProductCategory.Name AS 'Category'
+	*/
+
+FROM SalesLT.SalesOrderHeader
+
+JOIN SalesLT.SalesOrderDetail
+ON SalesLT.SalesOrderDetail.SalesOrderID = SalesLT.SalesOrderHeader.SalesOrderID
+
+JOIN SalesLT.Address
+ON SalesLT.Address.AddressID = SalesLT.SalesOrderHeader.ShipToAddressID
+
+JOIN SalesLT.Product
+ON SalesLT.Product.ProductID = SalesLT.SalesOrderDetail.ProductID
+
+JOIN SalesLT.ProductCategory
+ON SalesLT.ProductCategory.ProductCategoryID = SalesLT.Product.ProductCategoryID
+
+WHERE 
+	SalesLT.ProductCategory.ParentProductCategoryID = 2
+	AND SalesLT.Address.CountryRegion LIKE '%Canada%'
+	AND SalesLT.Address.StateProvince LIKE '%Quebec%'
+
+--ans: 108
--- Q19.sql
+++ Q19.sql
@@ -1,5 +1,17 @@
 /*
-For each sales order, compute the cost of freight per unit (across all its products). Sort your result by the computed cost descendingly.
+For each sales order, compute the cost of freight per unit (across all its products). 
+Sort your result by the computed cost descendingly.
 
 Enter into Canvas the cost found above for the order in row 53, padded and rounded to 2dp.
 */
+
+SELECT 
+    SalesLT.SalesOrderHeader.SalesOrderID,
+    SalesLT.SalesOrderDetail.ProductID,
+    SalesLT.SalesOrderHeader.Freight AS 'Total Freight Cost',
+    SalesLT.SalesOrderDetail.OrderQty AS 'Qty each product'
+
+FROM SalesLT.SalesOrderHeader
+
+JOIN SalesLT.SalesOrderDetail
+ON SalesLT.SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
