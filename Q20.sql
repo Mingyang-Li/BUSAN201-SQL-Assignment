25b8cd93bf27b43e8cb610151626ed757ec49ee5
Mingyang-Li <mingyangli118@gmail.com>
2021-05-16 19:34:01 +1200

    modified q20

--- Q20.sql
+++ Q20.sql
@@ -1,5 +1,35 @@
 /*
-Find how many units of each product have been ordered as at 11:59:59pm, 31st July 2017. Only include products available for sale as at 11:59:59pm, 31st July 2017. Products available for sale are those which have begun to sell, and are not yet ended or discontinued. Dates are provided in the SellStartDate, SellEndDate, and DiscontinuedDate columns.
+*** QUESTION 20 (TWENTY) ***
+Find how many units of each product have been ordered as at 11:59:59pm, 31st July 2017. 
+Only include products available for sale as at 11:59:59pm, 31st July 2017. 
+Products available for sale are those which have begun to sell, and are not yet ended or discontinued. 
+Dates are provided in the SellStartDate, SellEndDate, and DiscontinuedDate columns.
 
 From the resulting list, enter into Canvas how many unique ProductNumbers have exceeded 39 units.
 */
+
+SELECT 
+	--SalesLT.SalesOrderHeader.OrderDate,
+	--SalesLT.SalesOrderHeader.SalesOrderID,
+    --SalesLT.SalesOrderDetail.ProductID,
+	--SalesLT.Product.SellStartDate,
+	--SalesLT.Product.SellEndDate,
+	--SalesLT.Product.DiscontinuedDate
+	SUM(SalesLT.SalesOrderDetail.OrderQty) AS 'Sum of units ordered'
+FROM SalesLT.SalesOrderHeader
+
+JOIN SalesLT.SalesOrderDetail
+ON SalesLT.SalesOrderDetail.SalesOrderID = SalesLT.SalesOrderHeader.SalesOrderID
+
+JOIN SalesLT.Product
+ON SalesLT.Product.ProductID = SalesLT.SalesOrderDetail.ProductID
+
+WHERE 
+	SalesLT.SalesOrderHeader.OrderDate <'2017-08-01' -- order date before 31/07/2017
+    AND SalesLT.Product.SellStartDate <'2017-08-01' -- product avail for sale as at 11:59:59pm,, 31/07/2017
+	AND SalesLT.Product.SellStartDate IS NOT NULL
+	AND SalesLT.Product.SellEndDate IS NULL
+	AND SalesLT.Product.DiscontinuedDate IS NULL
+GROUP BY SalesLT.Product.ProductID
+HAVING SUM(SalesLT.SalesOrderDetail.OrderQty) > 39
+--ANS: 20
