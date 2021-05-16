8423ccc23971e3f4975192d89ce68d2064932378
Mingyang-Li <mingyangli118@gmail.com>
2021-05-16 18:52:36 +1200

    hald done q19

--- Q19.sql
+++ Q19.sql
@@ -1,4 +1,5 @@
 /*
+Q16
 For each sales order, compute the cost of freight per unit (across all its products). 
 Sort your result by the computed cost descendingly.
 
@@ -7,11 +8,14 @@ Enter into Canvas the cost found above for the order in row 53, padded and round
 
 SELECT 
     SalesLT.SalesOrderHeader.SalesOrderID,
-    SalesLT.SalesOrderDetail.ProductID,
-    SalesLT.SalesOrderHeader.Freight AS 'Total Freight Cost',
-    SalesLT.SalesOrderDetail.OrderQty AS 'Qty each product'
+    --SalesLT.SalesOrderDetail.ProductID,
+    --SalesLT.SalesOrderHeader.Freight AS 'Total Freight Cost',
+    SUM(SalesLT.SalesOrderDetail.OrderQty) AS 'Unites per order'
+	--SalesLT.SalesOrderHeader.Freight / SUM(SalesLT.SalesOrderDetail.OrderQty) AS 'Average Freight per unit'
 
 FROM SalesLT.SalesOrderHeader
 
 JOIN SalesLT.SalesOrderDetail
 ON SalesLT.SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+
+GROUP BY SalesLT.SalesOrderHeader.SalesOrderID
